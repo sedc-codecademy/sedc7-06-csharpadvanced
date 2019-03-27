80dbdcd92768e7921ab10094fee87df7efe56b16
Miodrag <cekicmiodrag@yahoo.com>
2019-03-27 13:35:15 +0100

    Initial code base for Class 2

---
+++ g6/CSharpAdvanced - Class2 (Preparation)/Class2Demo/Class2Demo/Graphics.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Class2Demo
+{
+    public class Graphics
+    {
+        public void DrawShapes(List<Shape> shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+}
---
+++ g6/CSharpAdvanced - Class2 (Preparation)/Class2Demo/Class2Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Class2Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>();
+            shapes.Add(new Shape() { Height = 10, Width = 10, Color = "Red", Type = ShapeType.Circle });
+            shapes.Add(new Shape() { Height = 100, Width = 100, Color = "Green", Type = ShapeType.Triangle });
+            shapes.Add(new Shape() { Height = 200, Width = 300, Color = "Blue", Type = ShapeType.Circle });
+
+            var graphics = new Graphics();
+            graphics.DrawShapes(shapes);
+
+            Console.ReadLine();
+        }
+    }
+}
