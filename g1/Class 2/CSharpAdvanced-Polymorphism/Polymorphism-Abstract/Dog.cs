5d1dc1314d3f0a75ec130f33fdfbcc57997064c5
unknown <you@example.com>
2019-03-28 22:05:18 +0100

    Added class 2

---
+++ g1/Class 2/CSharpAdvanced-Polymorphism/Polymorphism-Abstract/Dog.cs
@@ -0,0 +1,20 @@
+﻿namespace Polymorphism_Abstract
+{
+    class Dog : Animal
+    {
+        public override bool Feathers { get; set; } = false;
+        public override string Describe()
+        {
+            return base.Describe() + ": This is about Dogs.";
+        }
+        public override string Features()
+        {
+            return "Can bark.";
+        }
+        public override bool HasFeathers()
+        {
+            return Feathers;
+        }
+        //You cannot declare an abstract method outside an abstract class 
+    }
+}
---
+++ g1/Class 2/CSharpAdvanced-Polymorphism/Polymorphism-Dynamic/ElectricCar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Polymorphism_Dynamic
+{
+    public class ElectricCar : ICar
+    {
+        public int BatteryLifeMonths { get; set; }
+        public int BatteryDuration { get; set; }
+        public string MotorNumber { get; set; }
+        public ElectricCar()
+        {
+            MotorNumber = this.MotorNumber + "-E"; // E - for electric cars
+        }
+        public ElectricCar(string engineNumber)
+        {
+            MotorNumber = engineNumber + "-E";
+        }
+        public void Refuel()
+        {
+            Console.WriteLine("Charging battery");
+        }
+    }
+}
