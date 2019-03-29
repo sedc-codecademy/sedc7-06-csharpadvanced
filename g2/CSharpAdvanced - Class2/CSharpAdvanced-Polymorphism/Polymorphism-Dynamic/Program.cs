5be28f3dc2399190e637a08e7ff6d773eed90340
Igor Micev <igor.micev@navetti.com>
2019-03-29 21:33:05 +0100

    CSharpAdvanced - Class2/CSAdvanded-Class2

---
+++ g2/CSharpAdvanced - Class2/CSharpAdvanced-Polymorphism/Polymorphism-Dynamic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar[] cars = {
+                new NormalCar("VW234KJH234"),
+                new ElectricCar("VW2ERT2MM34"),
+                new HybridCar("VW2ERT23JKL0")
+            };
+            cars[0].Refuel(); Console.WriteLine(cars[0].MotorNumber);
+            cars[1].Refuel(); Console.WriteLine(cars[1].MotorNumber);
+            cars[2].Refuel(); Console.WriteLine(cars[2].MotorNumber);
+        }
+    }
+}
---
+++ g2/CSharpAdvanced - Class2/ReadMe.txt
@@ -0,0 +1,2 @@
+All demos and exercises are in slide #7.
+The trainer has to define tasks of each section for exercising.
