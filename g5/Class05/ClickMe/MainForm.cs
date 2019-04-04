032c1d9a5cdf8cf1b5000d2c559970b503c3539b
Wekoslav Stefanovski <swekster@gmail.com>
2019-04-04 19:27:47 +0200

    Added windows forms example

---
+++ g5/Class05/ClickMe/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickMe
+{
+    public partial class MainForm : Form
+    {
+
+        public int Counter { get; set; }
+
+        public MainForm()
+        {
+            Counter = 0;
+            InitializeComponent();
+        }
+
+        private void WhenButtonClicked(object sender, EventArgs e)
+        {
+            Counter += 1;
+            lblResult.Text = $"I've been clicked {Counter} times!";
+
+            if (Counter >= 10)
+            {
+                // btnClickMe.Enabled = false;
+                btnClickMe.Click -= WhenButtonClicked;
+            }
+        }
+    }
+}
