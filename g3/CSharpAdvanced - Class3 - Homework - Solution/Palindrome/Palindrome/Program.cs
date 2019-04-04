eee5ed8e228d437a158a0eaeaf09b89b2345a9f9
Igor Micev <igor.micev@navetti.com>
2019-04-04 21:26:37 +0200

    Commit

---
+++ g3/CSharpAdvanced - Class3 - Homework - Solution/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome {
+    class Program {
+        static void Main(string[] args) {
+
+            string phrase;
+            phrase = "Madam, I'm Adam";
+            Console.WriteLine(phrase.IsPalindrom()); //True            
+            phrase = "Madam, I am Adam";
+            Console.WriteLine(phrase.IsPalindrom()); //False
+            phrase = "Refer, refer!";
+            Console.WriteLine(phrase.IsPalindrom()); //True
+
+            double numeric;
+            numeric = 123.321;
+            Console.WriteLine(numeric.IsPalindrom()); //True
+
+            int integer;
+            integer = 11211;
+            Console.WriteLine(integer.IsPalindrom()); //True      
+
+            long bigInt;
+            bigInt = 12345654321;
+            Console.WriteLine(bigInt.IsPalindrom()); //True 
+
+            Int16 i16 = 121;
+            Console.WriteLine(i16.IsPalindrom());
+
+        }
+    }
+}
