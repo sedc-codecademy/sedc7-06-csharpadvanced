f0180e70be4340d830de013f822b0f832beb6bd6
dragan <dragang@iborn.net>
2019-04-05 21:21:22 +0200

    class 4 code

---
+++ g4/Class 4/SEDC.Class04/ExtensionMethods/Db.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class Db
+    {
+        public static List<string> Names = new List<string>() {
+            "Bob", "Jill", "Greg"
+        };
+        public static List<Human> Humans = new List<Human>();
+    }
+}
