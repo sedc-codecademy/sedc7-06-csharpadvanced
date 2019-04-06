fe40b247f34f486d748d7661e7a804a2755ed058
dragan <dragang@iborn.net>
2019-04-06 16:19:33 +0200

    Class 5 code

---
+++ g4/Class 5/SEDC.Class05/Generics/Program.cs
@@ -0,0 +1,75 @@
+﻿using Generics.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public void Greet()
+        {
+            Console.WriteLine($"Hey there {Name}");
+        }
+    }
+    public class Programmer : Human
+    {
+        public string ProgrammingLanguage { get; set; }
+    } 
+    public class Tester : Human
+    {
+        public string BugsFound { get; set; }
+    }
+
+    class Program
+    {
+        public static void CallHumans<T>(List<T> humans) where T : Human
+        {
+            foreach (T item in humans)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Not generic helper testing
+            NotGenericListHelper NotGeneric = new NotGenericListHelper();
+            List<string> strings = new List<string>() { "str1", "str2", "str3" };
+            List<int> ints = new List<int>() { 5, 22, -18 };
+            List<bool> bools = new List<bool> { true, false, true };
+            NotGeneric.GoThroughStrings(strings);
+            NotGeneric.GetInfoForStrings(strings);
+            NotGeneric.GoThroughInts(ints);
+            NotGeneric.GetInfoForInts(ints);
+
+            // Generic method outside of generic class
+            Console.WriteLine("HERE ARE THE RESULTS OF THE GENERIC METHOD");
+            Console.WriteLine(ReturnTypeString("Hey"));
+            Console.WriteLine(ReturnTypeString<int>(23)); // This is also valid
+            Console.WriteLine(ReturnTypeString(new List<bool>() { true, false }));
+
+
+            // Generic methods in generic class
+            // non-static calling
+            //GenericListHelper<string> genericHelper1 = new GenericListHelper<string>();
+            //GenericListHelper<int> genericHelper2 = new GenericListHelper<int>();
+            //GenericListHelper<bool> genericHelper3 = new GenericListHelper<bool>();
+            //genericHelper1.GoThrough(strings);
+            //genericHelper1.GetInfo(strings);
+            //genericHelper2.GoThrough(ints);
+            //genericHelper2.GetInfo(ints);
+            //genericHelper3.GoThrough(bools);
+            //genericHelper3.GetInfo(bools);
+            GenericListHelper<string>.GoThrough(strings);
+
+            Console.ReadLine();
+        }
+        public static string ReturnTypeString<T>(T something)
+        {
+            return $"This thing is of type {something.GetType().FullName}!";
+        }
+    }
+}
---
+++ g4/Class 5/SEDC.Class05/Revision/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revision
+{
+    public static class Baza
+    {
+        private static List<string> stringovi1 = new List<string>()
+        {"eden", "dva", "tri" };
+        private static List<string> stringovi2 = new List<string>()
+        {"eden", "dva", "tri" };
+        public static List<Dog> dogs = new List<Dog>()
+        {
+            new Dog(){Name = "Bob", Age=2, Race="Dzuki"},
+            new Dog(){Name = "Berta", Age=3, Race="Pekinezer"},
+            new Dog(){Name = "Bak", Age=4, Race="Kuciste"}
+        };
+        public static void AddStringToStrings(string myString)
+        {
+            stringovi1.Add(myString);
+            stringovi2.Add(myString);
+        }
+    }
+    public class Dog
+    {
+        // Auto properties
+        public string Name { get; set; }
+        public string Race { get; set; }
+
+        // Custom property
+        private int _Age;
+        public int Age
+        {
+            get // rex.Age;
+            {
+                return _Age;
+            }
+            set // rex.Age = 4;
+            {
+                if(value < 0)
+                {
+                    Console.WriteLine("Ne moze minus godini!");
+                    Console.WriteLine("Age e ne promeneto! Probaj pak!");
+                    return;
+                }
+                _Age = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog rex = new Dog() { Name = "Rex", Age = 6, Race = "German Shepard" };
+            Console.WriteLine(rex.Age);
+            Baza.AddStringToStrings("cetiri");
+            Baza.dogs.Add(rex);
+            Baza.dogs.Add(new Dog() { Name = "xx", Age = 1, Race = "xxx" });
+            Console.ReadLine();
+        }
+    }
+}
