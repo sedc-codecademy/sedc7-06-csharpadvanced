390770f46385e618acc243f5b50e660d9615d492
dragan <dragang@iborn.net>
2019-04-08 14:42:26 +0200

    Homework pt1

---
+++ g4/Homework/SEDC.PcPartsApp/Entities/Classes/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Configuration : Item
+    {
+        public string Title { get; set; }
+        public ConfigurationType Type { get; set; }
+        public List<Module> Modules { get; set; }
+        public Configuration()
+        {
+            Modules = new List<Module>();
+        }
+        public Configuration(int id, double price, double discount, string title, ConfigurationType type, List<Module> modules)
+            : base(id, price, discount)
+        {
+            Title = title;
+            Type = type;
+            Modules = modules;
+        }
+    }
+}
---
+++ g4/Homework/SEDC.PcPartsApp/Entities/Enums/PartType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public enum PartType
+    {
+        // Proccessing
+        Cpu,
+        CpuCooler,
+        // Graphics
+        Gpu,
+        GpuCooler,
+        // Casing
+        Case,
+        PowerSuply,
+        // MainBoard
+        MotherBoard,
+        ConnectionCable,
+        PowerCable,
+        // Memory
+        SSD,
+        HDD,
+        RAM,
+        // Other
+        Monitor,
+        Mouse,
+        Keyboard
+    }
+}
