c5d350eae39a0bf73e6899a4a0a949417c8db3a2
dragan <dragang@iborn.net>
2019-04-15 21:08:04 +0200

    Class 8 Code

---
+++ g4/Class 8/Linq/Linq/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } // List<string>
+        public string LastName { get; set; }
+        public int Age { get; set; } // List<int>
+        public bool IsPartTime { get; set; } // List<bool>
+        public List<Subject> Subjects { get; set; } // List<List<Subject>>
+        public Student()
+        {
+
+        }
+        public Student(int id, string first, string last, int age, bool partTime)
+        {
+            Id = id;
+            FirstName = first;
+            LastName = last;
+            Age = age;
+            IsPartTime = partTime;
+        }
+    }
+}
---
+++ g4/Class 8/Linq/RevisionAnonMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisionAnonMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Func
+            Func<int, int, string> Sum = (x, y) => $"The result is : {x + y}";
+            Console.WriteLine(Sum(2,5));
+            Func<string> HelloWorld = () => "Hello world";
+            Func<int, string> IsEven = x => {
+                Console.WriteLine("Processing...");
+                if (x % 2 == 0) return "Even";
+                return "Odd";
+            };
+            Console.WriteLine(IsEven(2));
+            // Action
+            Action<string> SayHello = x => Console.WriteLine($"Hello {x}");
+            Action SayHelloWorld = () => Console.WriteLine("Hello world!");
+            SayHello("Bob");
+            SayHelloWorld();
+            Console.ReadLine();
+        }
+    }
+}
