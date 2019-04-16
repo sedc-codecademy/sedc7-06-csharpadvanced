47b3caac658509eec99e06bc057a7c0280b98889
Igor Micev <igor.micev@navetti.com>
2019-04-17 00:13:12 +0200

    CSharpAdvanced - Class9

---
+++ g3/CSharpAdvnaced - Class9/CSharpAdvanced-Class9/BasicOperators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOperators {
+    class Program {
+        static void Main(string[] args) {
+            Box b1 = new Box(1, 2, 3);
+            b1.PrintBox();
+            Box b2 = new Box(2, 3, 4);
+            b2.PrintBox();
+            Box b3;
+            b3 = b1 + b2;
+            b3.PrintBox();
+            Console.WriteLine(b3.Volume());
+
+            b3--;
+            b3.PrintBox();
+            Console.WriteLine(b3.Volume());
+
+            Box b4;
+            b4 = -b2;
+            b4.PrintBox();
+            Console.WriteLine(b4.Volume());
+
+            Box b5 = new Box(0, 0, 0);
+            Console.WriteLine(!b5);
+            Box b6 = new Box(1, 1, 1);
+            Console.WriteLine(!b6);
+
+        }
+    }
+}
---
+++ g3/CSharpAdvnaced - Class9/CSharpAdvanced-Class9/CustomersSorting/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersSorting {
+    public class Customer : IComparable<Customer> 
+    { 
+        public string Name { get; set; } 
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int MonthlySpend { get; set; }
+
+        public Customer() { }
+        public Customer(string name, string address, string city, int monthlySpend) {
+            this.Name = name; this.Address = address; this.City = city; this.MonthlySpend = monthlySpend;
+        }
+        public void PrintCustomer() {
+            Console.WriteLine($"Customer: {Name}, {Address}, {City}, {MonthlySpend}");
+        }
+        public int CompareTo(Customer c) {
+            return this.MonthlySpend.CompareTo(c.MonthlySpend);
+        }
+    }
+
+    public class CustomerMonthlySpendComparer : IComparer<Customer>  {
+        public int Compare(Customer c1, Customer c2) {
+            return c1.MonthlySpend - c2.MonthlySpend;
+        }
+
+       
+    }
+    public class CustomerCityComparer : IComparer<Customer> {
+        public int Compare(Customer c1, Customer c2) {
+            return string.Compare(c1.City, c2.City);
+        }
+    }
+     
+
+}
