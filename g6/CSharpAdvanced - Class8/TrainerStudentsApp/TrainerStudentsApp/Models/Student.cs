a2d70155a49a0ff8d7a54b05f96e6b32990ccb8b
Andrej Chichak <Andrej.Chichakovski@seavus.com>
2019-04-17 18:06:08 +0200

    Solved TrainerStudents excercise

---
+++ g6/CSharpAdvanced - Class8/TrainerStudentsApp/TrainerStudentsApp/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TrainerStudentsApp.EventModels;
+
+namespace TrainerStudentsApp.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Student(int id, string firstName, string lastName)
+        {
+            StudentID = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void ListenTrainerAnnouncement(object source, ExamMarkDetails markInfo)
+        {
+            if (markInfo.Student.StudentID.Equals(StudentID))
+            {
+                if (markInfo.Points < 50)
+                {
+                    Console.WriteLine("Oh no, I failed the exam. :( ");
+                }
+                else
+                {
+                    Console.WriteLine("Great, I have passed the exam! :) ");
+                }
+            }
+        }
+    }
+}
---
+++ g6/CSharpAdvanced - Class8/TrainerStudentsApp/TrainerStudentsApp/Models/Trainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TrainerStudentsApp.EventModels;
+
+namespace TrainerStudentsApp.Models
+{
+    public delegate void AnnounceMarkEventHandler(object source, ExamMarkDetails args);
+
+    public class Trainer
+    {
+        public event AnnounceMarkEventHandler AnnounceMark;
+        public void Announce(ExamMarkDetails studentMarkInfo)
+        {
+            Console.WriteLine($"Trainer: Student with id: {studentMarkInfo.Student.StudentID} - {studentMarkInfo.Student.FirstName} {studentMarkInfo.Student.LastName}, Points: {studentMarkInfo.Points}");
+
+            AnnounceMark?.Invoke(this, studentMarkInfo);
+        }
+    }
+}
