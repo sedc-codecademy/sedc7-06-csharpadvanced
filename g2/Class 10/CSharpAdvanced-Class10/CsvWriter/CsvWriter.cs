5f8f950c6fa05051e7f4f9da15149b61229705bb
Igor Mitkovski <igor.mitkovski@outlook.com>
2019-04-19 21:09:30 +0200

    Add Class 10

---
+++ g2/Class 10/CSharpAdvanced-Class10/CsvWriter/CsvWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CsvWriter {
+    public class CsvWriter : StreamWriter
+    {
+        public char Delimiter { get; set; }
+        public char Quote { get; set; }
+        public string CommonToken { get; set; }
+
+        public CsvWriter(string path, bool append, Encoding encoding): base(path, append, encoding) {
+            Delimiter = ',';
+            Quote = '"';
+            CommonToken = "## ";
+        }
+        public CsvWriter(string file) : this(file, false, Encoding.UTF8) { }
+        public CsvWriter(string file, bool append) : this(file, append, Encoding.UTF8) { }
+
+    }
+}
---
+++ g2/Class 10/Demo/CsvWriterReader/Program.cs
@@ -0,0 +1,69 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvWriterReader
+{
+    class Program
+    {
+        public class Customer
+        {
+            public int CustomerID { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+
+            public void Print()
+            {
+                Console.WriteLine($"{CustomerID}, {Name}, {Address}, {City}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            string path = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())));
+            path = Path.Combine(path, @"Files\CsvWriterReader\Customers.csv");
+            //string path = @"C:\Users\igor.micev\Desktop\Customers.csv";
+
+            #region Writing csv
+            Console.WriteLine("--------------- Writing csv ---------------");
+            List<Customer> customersToWrite = new List<Customer>
+            {
+                new Customer{CustomerID = 1, Name ="igor", Address = "Roza Luxemburg",City="Skopje"},
+                new Customer{CustomerID = 2, Name = "ivan", Address = "Partizanska", City = "Ohrid"},
+                new Customer{CustomerID = 3, Name = "dejan", Address = "Helsinshka", City = "Stip"}
+            };
+
+            using (StreamWriter writer = new StreamWriter(path))
+            using (CsvWriter csv = new CsvWriter(writer))
+            {
+                csv.Configuration.Delimiter = ";";
+                csv.WriteRecords(customersToWrite);
+            }
+            #endregion
+            
+
+
+            #region Reading csv
+            Console.WriteLine("--------------- Reading from csv into class list---------------");
+            List<Customer> customersToRead = new List<Customer>();
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader))
+            {
+                csv.Configuration.Delimiter = ";";
+                customersToRead = csv.GetRecords<Customer>().ToList();
+                foreach (Customer cust in customersToRead)
+                {
+                    cust.Print();
+                }
+            }
+            #endregion
+
+
+            Console.ReadLine();
+        }
+    }
+}
