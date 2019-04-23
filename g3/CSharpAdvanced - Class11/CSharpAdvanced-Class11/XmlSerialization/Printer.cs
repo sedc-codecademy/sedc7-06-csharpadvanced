1056a12a35ec28f7dd710189319f9308efdb0ef2
Igor Micev <igor.micev@navetti.com>
2019-04-23 21:33:18 +0200

    CSharpAdvanced - Class11

---
+++ g3/CSharpAdvanced - Class11/CSharpAdvanced-Class11/XmlSerialization/Printer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XmlSerialization {
+
+    public enum PrinterStatus {
+        [XmlEnum("On-line")]
+        OK =0,
+        [XmlEnum("Off-line")]
+        Offline =1,
+        [XmlEnum("No paper")]
+        PaperEmpty =2,
+        [XmlEnum("No toner")]
+        TonerExhausted =3,
+        [XmlEnum("Jammed")]
+        PaperJam =4
+    }
+
+    public class Printer {
+        public string Name { get; set; }
+        public PrinterStatus Status { get; set; }
+    }
+
+}
