39f79f9c713010384e9025ae1e40a0a7945c12de
Martin <panovski62@gmail.com>
2019-05-02 21:07:27 +0200

    Added class 13

---
+++ g3/CSharpAdvanced - Class13/AsyncAwait/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SendMessage("Hello there, we are SEDC students!");
+            //Task x = SendMessageAsync("Hello there, we are SEDC students!");
+
+            //Console.WriteLine($"{x.Status}");
+
+            //ShowAd("Shampoo");
+
+            Task y = MainThread();
+
+            Console.ReadLine();
+
+        }
+
+        public static async Task MainThread()
+        {
+            await SendMessageAsync("Our message to be sent!");
+            ShowAd("Potato");
+        }
+
+        public static void SendMessage(string message)
+        {
+            Console.WriteLine("Sending message...");
+            Thread.Sleep(5000);
+            Console.WriteLine($"The message '{message}' is sent!");
+        }
+
+        public static async Task SendMessageAsync(string message)
+        {
+            Console.WriteLine("Sending message...");
+            await Task.Run(() =>
+            {
+                Thread.Sleep(7000);
+                Console.WriteLine($"The message '{message}' is sent!");
+            });
+        }
+
+        public static void ShowAd(string commercialProduct)
+        {
+            Console.WriteLine("While you are waiting, let us show our ad:");
+            Thread.Sleep(2000);
+            Console.WriteLine("By the best product in the world");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(commercialProduct);
+            Console.ResetColor();
+            Thread.Sleep(2000);
+            Console.WriteLine(" now and get ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Thread.Sleep(2000);
+            Console.WriteLine("FREE");
+            Console.ResetColor();
+            Console.WriteLine("shipping!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ g3/CSharpAdvanced - Class13/AsyncAwaitExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task myTask = MyAsyncMethod();
+            Console.WriteLine("Cursor move to next line of execution without waiting for MyAsyncMethod()");
+            Console.WriteLine("Now waiting for the task to be finished!");
+            Task.WaitAll(myTask);
+            Console.WriteLine("Exiting command line!");
+
+            Console.ReadLine();
+        }
+
+        public static async Task MyAsyncMethod()
+        {
+            Task<int> longRunningTask = LongRunningOperation();
+
+            Console.WriteLine("Independet Works of now executes in MyAsyncMethod()");
+
+            int result = await longRunningTask;
+
+            Console.WriteLine("The result is {0}", result);
+        }
+
+        public static async Task<int> LongRunningOperation()
+        {
+            Console.WriteLine("LongRunningOperation() started!");
+            await Task.Delay(2000);
+            Console.WriteLine("LongRunningOperation() finished after 2 seconds!");
+            return 1;
+        }
+    }
+}
