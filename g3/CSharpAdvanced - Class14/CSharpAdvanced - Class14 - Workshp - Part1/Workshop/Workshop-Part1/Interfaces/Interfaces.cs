53f43ba911a025841959e84672b8f87efabd1cb3
Igor Micev <igor.micev@navetti.com>
2019-05-04 17:23:24 +0200

    CSharpAdvanced - Class14 - Workshp - Part1

---
+++ g3/CSharpAdvanced - Class14/CSharpAdvanced - Class14 - Workshp - Part1/Workshop/Workshop-Part1/Interfaces/Interfaces.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_Part1.Interfaces {
+    public interface IWorkable {
+        void Work();
+    }
+    public interface IEatable {
+        void Eat();
+    }
+    public interface IManageable {
+        void Manage();
+    }
+}
