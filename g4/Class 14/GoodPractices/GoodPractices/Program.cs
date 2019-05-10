541824eaefe9b16d2d7ab388267836771abff3ae
dragan <dragang@iborn.net>
2019-05-10 17:22:35 +0200

    Class 14 materials

---
+++ g4/Class 14/GoodPractices/GoodPractices/Program.cs
@@ -0,0 +1,18 @@
+﻿using GoodPractices.Principles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
